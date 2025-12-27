d62f01cbc64d67a7d8e7df95849d655dee1b0480
jihanurrahman33 <jihanurrahman33@gmail.com>
2025-12-27 15:16:29 +0600

    Implement FoodsPage with data fetching; add NavBar component and update layout metadata

--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
-const FoodsPage = () => {
-  return <div>Foods</div>;
+const getFoods = async () => {
+  const res = await fetch(
+    "https://taxi-kitchen-api.vercel.app/api/v1/foods/random",
+    {}
+  );
+  const data = await res.json();
+  return data.foods || [];
+};
+const FoodsPage = async () => {
+  const foods = await getFoods();
+  return (
+    <div>
+      <h2>Total {foods.length} Foods Found.</h2>
+    </div>
+  );
 };
 
 export default FoodsPage;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  const links = [
+    { name: "Home", path: "/" },
+    { name: "Foods", path: "/foods" },
+    { name: "Reviews", path: "/reviews" },
+  ];
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-1">
+          <Link href="/" className="btn btn-ghost text-xl">
+            Food Res
+          </Link>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            {links.map((link) => (
+              <li key={link.path}>
+                <Link href={link.path}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
